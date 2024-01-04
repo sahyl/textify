d88bd52475822fe9013cb70089fd8075545503e8
Sahil Khan <mohammedsahilkhan.msk@gmail.com>
2024-01-04 20:04:48 +0530

    textify

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import './App.css';
+import Navbar from './Component/Navbar';
+import TextForm from './Component/TextForm';
+import About from './Component/About';
+import React, { useState } from 'react';
+import Alert from './Component/Alert';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+ 
+function App() {
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+          setAlert(null);
+      }, 1500);
+  }
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+    }
+  }
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar
+          title="Textify"
+          aboutText="About Us"
+          mode={mode}
+          toggleMode={toggleMode}
+        />
+        <Alert alert={alert} />
+        <div className="container my-4" mode={mode}>
+          <Routes>
+            <Route exact path="/about" element={<About />}></Route>
+            <Route
+              exact path="/"
+              element={
+                <TextForm
+                  showAlert={showAlert}
+                  heading="Enter Text to analyze "
+                  mode={mode}
+                />
+              }
+            ></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
