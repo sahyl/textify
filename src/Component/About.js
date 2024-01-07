edcc5f54a61df3e731588eb809c09896ebeca2a7
Mohammed Sahil Khan <mohammedsahilkhan.msk@gmail.com>
2024-01-07 20:47:29 +0530

    about bg color , daark light mode

--- src/Component/About.js
+++ src/Component/About.js
@@ -7,13 +7,13 @@ export default function About(props) {
     //     backgroundColor: 'white'
     // }) 
     let myStyle = {
-        color: props.mode ==='dark'?'white':'#042743',
-        backgroundColor: props.mode ==='dark'?'rgb(36 74 104)':'white', 
+        color: props.mode ==='dark'?'white':'black',
+        backgroundColor: props.mode ==='dark'?'#13466e':'white', 
     }
     
     return (
-        <div className="container" myStyle>
-            <h1 className="my-3" style={{color: props.mode ==='dark'?'white':'#042743'}}>About Us</h1>
+        <div className="container">
+            <h1 className="my-3" style={{color: props.mode ==='dark'?'white':'black'}}>About Us</h1>
             <div className="accordion" id="accordionExample">
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingOne">
@@ -23,7 +23,7 @@ export default function About(props) {
                     </h2>
                     <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                     <div className="accordion-body" style={myStyle}>
-                    Textify gives you a way to analyze your text quickly and efficiently. Be it word count, character count or 
+                    Textutils gives you a way to analyze your text quickly and efficiently. Be it word count, character count or 
 
                     </div>
                     </div>
@@ -36,7 +36,7 @@ export default function About(props) {
                     </h2>
                     <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                     <div className="accordion-body" style={myStyle}>
-                    Textify is a free character counter tool that provides instant character count & word count statistics for a given text. Textify reports the number of words and characters. Thus it is suitable for writing text with word/ character limit.
+                    TextUtils is a free character counter tool that provides instant character count & word count statistics for a given text. TextUtils reports the number of words and characters. Thus it is suitable for writing text with word/ character limit.
                     </div>
                     </div>
                 </div>
@@ -58,3 +58,5 @@ export default function About(props) {
         </div>
     )
 }
+
+
